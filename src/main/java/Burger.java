98bbb9b9171edb7670ba9797a7cb6020e703caf6
student <samoilovid951@gmail.com>
2019-10-06 09:49:23 +0300

    fixes #2

---
+++ src/main/java/Burger.java
@@ -0,0 +1,12 @@
+public class Burger {
+    String name;
+    long id;
+    String avatar;
+    int price;
+    int grams;
+    int kcal;
+    boolean inBasket;
+    double protein;
+    double fat;
+    double carbohydrates;
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        Burger chefburger = new Burger();
+        chefburger.id = 7;
+        chefburger.avatar = "https://s82079.cdn.ngenix.net/5ryrwTKuHTR5tX81w878Wuud.png?dw=230";
+        chefburger.grams = 213;
+        chefburger.kcal = 236;
+        chefburger.price = 129;
+        chefburger.protein = 13;
+        chefburger.fat = 10.7;
+        chefburger.carbohydrates = 22;
+
+        Burger cheeseburger = new Burger();
+        cheeseburger.id = 2;
+        cheeseburger.avatar = "https://s82079.cdn.ngenix.net/9VW3RQfY33AnJi7Yu38nKiHc.png?dw=230";
+        cheeseburger.grams = 167;
+        cheeseburger.kcal = 237;
+        cheeseburger.price = 69;
+        cheeseburger.protein = 12.2;
+        cheeseburger.fat = 8.6;
+        cheeseburger.carbohydrates = 26.6;
+    }
+}
